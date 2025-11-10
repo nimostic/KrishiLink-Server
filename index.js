faa4468f630bf0365ffba035c60d68f55095b6e3
Abu Nayeem Riyad <rionimo5@gmail.com>
2025-11-11 02:38:47 +0600

    create crops and get latest crops api created

--- index.js
+++ index.js
@@ -1,16 +1,68 @@
-const express = require('express')
-const app = express()
-const port = 3000
-
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5000;
 
 //middleware
+app.use(cors());
+app.use(express.json());
 
+//mongodb connection
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gubl8vg.mongodb.net/?appName=Cluster0`;
 
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+app.get("/", (req, res) => {
+  res.send("Krishi server is running...");
+});
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+async function run() {
+  try {
+    // await client.connect();
+   
+
+    const db = client.db("krishi");
+    const usersCollection = db.collection("users");
+    const cropsCollection = db.collection("crops");
+
+    // add a crop
+    app.post("/crops", async (req, res) => {
+      const crop = req.body;
+      console.log(crop);
+      const result = await cropsCollection.insertOne(crop);
+      res.send(result);
+    });
+
+    //get a crops
+    app.get("/crops", async (req, res) => {
+      const crops = await cropsCollection.find().toArray();
+      res.send(crops);
+    });
+
+    //latest 6 crops 
+    app.get('/latest-crops',async(req,res)=>{
+      const latestCrops = (await cropsCollection.find().sort({createdAt: -1 }).limit(6).toArray())
+      res.send(latestCrops)
+    })
+
+     console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  }
+   finally {
+    // await client.close();
+  }
+
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
