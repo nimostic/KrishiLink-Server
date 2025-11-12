e3ee2eb18d19cd66a66b42857ebdc4ae0dfd7416
Abu Nayeem Riyad <rionimo5@gmail.com>
2025-11-12 23:47:36 +0600

    added interest fetch

--- index.js
+++ index.js
@@ -39,10 +39,47 @@ async function run() {
       res.send(result);
     });
 
-    //get a crops
+    //send a interest form
+    app.post("/interests", async (req, res) => {
+      // console.log(req.body);
+      const { crop, interestData } = req.body;
+      console.log({ crop, interestData });
+      try {
+        const query = { _id: new ObjectId(crop._id) };
+        const update = {
+          $push: { interests: interestData },
+        };
+
+        // checking dupliacate interest send
+        const existingCrop = await cropsCollection.findOne(query);
+        const alreadyInterested = existingCrop.interests?.find(
+          (i) => i.buyerEmail === interestData.buyerEmail
+        );
+
+        if (alreadyInterested) {
+          return res.status(400).send({
+            message: "you already sent an interest for this crop",
+          });
+        }
+
+        const result = await cropsCollection.updateOne(query, update);
+        res.send(result);
+      } catch (error) {
+        res.status(500).send({ error: error.message });
+      }
+    });
+
+    //get crops
     app.get("/crops", async (req, res) => {
       const crops = await cropsCollection.find().toArray();
       res.send(crops);
+    });
+
+    //get a crops details
+    app.get("/crops/:id", async (req, res) => {
+      const { id } = req.params;
+      const result = await cropsCollection.findOne({ _id: new ObjectId(id) });
+      res.send(result);
     });
 
     // search a crop
@@ -77,12 +114,28 @@ async function run() {
     });
 
     //delete crop from my posts
-    app.delete('/delete/:id',async (req,res)=>{
-      const {id} = req.params
-      const result = await cropsCollection.deleteOne({_id : new ObjectId(id)})
-      res.send(result)
-    })
+    app.delete("/delete/:id", async (req, res) => {
+      const { id } = req.params;
+      const result = await cropsCollection.deleteOne({ _id: new ObjectId(id) });
+      res.send(result);
+    });
 
+    //edit my posts
+    app.patch("/edit/:id", async (req, res) => {
+      const { id } = req.params;
+      const updatedCrops = req.body;
+      console.log(updatedCrops);
+      delete updatedCrops._id;
+      const query = { _id: new ObjectId(id) };
+      try {
+        const result = await cropsCollection.updateOne(query, {
+          $set: updatedCrops,
+        });
+        res.send(result);
+      } catch (error) {
+        res.status(500).send({ error: error.message });
+      }
+    });
 
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
